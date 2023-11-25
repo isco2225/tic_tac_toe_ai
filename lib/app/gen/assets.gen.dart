ac4400f461d881737967ea47e721d56621e2c058
SemihYlmzz <ekkotaymmm@gmail.com>
2023-11-25 14:43:09 +0300

    # Rive initialized

---
+++ lib/app/gen/assets.gen.dart
@@ -0,0 +1,137 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+import 'package:rive/rive.dart';
+
+class $AssetsAnimationsGen {
+  const $AssetsAnimationsGen();
+
+  /// File path: assets/animations/tic_tac_toe_board.riv
+  RiveGenImage get ticTacToeBoard =>
+      const RiveGenImage('assets/animations/tic_tac_toe_board.riv');
+
+  /// List of all assets
+  List<RiveGenImage> get values => [ticTacToeBoard];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsAnimationsGen animations = $AssetsAnimationsGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class RiveGenImage {
+  const RiveGenImage(this._assetName);
+
+  final String _assetName;
+
+  RiveAnimation rive({
+    String? artboard,
+    List<String> animations = const [],
+    List<String> stateMachines = const [],
+    BoxFit? fit,
+    Alignment? alignment,
+    Widget? placeHolder,
+    bool antialiasing = true,
+    List<RiveAnimationController> controllers = const [],
+    OnInitCallback? onInit,
+  }) {
+    return RiveAnimation.asset(
+      _assetName,
+      artboard: artboard,
+      animations: animations,
+      stateMachines: stateMachines,
+      fit: fit,
+      alignment: alignment,
+      placeHolder: placeHolder,
+      antialiasing: antialiasing,
+      controllers: controllers,
+      onInit: onInit,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
--- lib/presentation/game/widgets/board.dart
+++ lib/presentation/game/widgets/board.dart
@@ -1,6 +1,8 @@
+import 'package:flame_rive/flame_rive.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_xox_app/app/cubits/game/game.dart';
+import 'package:flutter_xox_app/app/cubits/game/game_cubit.dart';
+import 'package:flutter_xox_app/app/gen/assets.gen.dart';
 
 class GameBoard extends StatefulWidget {
   const GameBoard({
@@ -12,50 +14,137 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> {
-  List<String> board = ['', '', '', '', '', '', '', '', ''];
-  String winner = '';
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
-  bool isTurnO = false;
+  late StateMachineController _boardController;
+  SMIInput<double>? box0;
+  SMIInput<double>? box1;
+  SMIInput<double>? box2;
+  SMIInput<double>? box3;
+  SMIInput<double>? box4;
+  SMIInput<double>? box5;
+  SMIInput<double>? box6;
+  SMIInput<double>? box7;
+  SMIInput<double>? box8;
+
+  void onInit(Artboard artboard, double turn) async {
+    _boardController = StateMachineController.fromArtboard(
+      artboard,
+      'State Machine 1',
+    )!;
+    artboard.addController(_boardController);
+
+    _boardController.addEventListener((riveEvent) {
+      final gameState = context.read<GameCubit>().state;
+      onRiveEvent(riveEvent, gameState.isTurnPlayer1 ? 1 : 2);
+    });
+
+    box0 = _boardController.findInput('0');
+    box1 = _boardController.findInput('1');
+    box2 = _boardController.findInput('2');
+    box3 = _boardController.findInput('3');
+    box4 = _boardController.findInput('4');
+    box5 = _boardController.findInput('5');
+    box6 = _boardController.findInput('6');
+    box7 = _boardController.findInput('7');
+    box8 = _boardController.findInput('8');
+  }
+
+  void onRiveEvent(RiveEvent event, double turn) {
+    int? clickedBox = detectEventBox(event.name);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (clickedBox != null) {
+        movement(clickedBox, turn);
+      }
+    });
+  }
+
+  // DELETABLE
+  // List<String> board = ['', '', '', '', '', '', '', '', ''];
+  // String winner = '';
+
+  // bool isTurnO = false;
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 9,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-      ),
-      itemBuilder: (BuildContext context, int index) {
-        final state = context.watch<GameCubit>().state;
-        final inBoard = state.board[index];
+    final gameState = context.watch<GameCubit>().state;
 
-        return GestureDetector(
-          onTap: () {
-            if (state.roundWinner != '') {
-              return;
-            }
-            context.read<GameCubit>().playerMovement(index);
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(
-                width: 3,
-                color: Colors.red,
-              ),
-              color: Colors.grey,
-            ),
-            child: Center(
-              child: Text(
-                inBoard,
-                style: const TextStyle(
-                  fontSize: 70,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
+    return RiveAnimation.asset(
+      Assets.animations.ticTacToeBoard.path,
+      stateMachines: const ['State Machine 1'],
+      onInit: (artBoard) {
+        onInit(
+          artBoard,
+          gameState.isTurnPlayer1 ? 1 : 2,
         );
       },
     );
+  }
+
+  int? detectEventBox(String name) {
+    return switch (name) {
+      'box0' => 0,
+      'box1' => 1,
+      'box2' => 2,
+      'box3' => 3,
+      'box4' => 4,
+      'box5' => 5,
+      'box6' => 6,
+      'box7' => 7,
+      'box8' => 8,
+      _ => null,
+    };
+  }
+
+  void movement(int clickedBoxIndex, double turnIndex) {
+    void playOnCubit() {
+      context.read<GameCubit>().playerMovement(clickedBoxIndex);
+    }
+
+    switch (clickedBoxIndex) {
+      case 0:
+        box0?.change(turnIndex);
+        playOnCubit();
+        break;
+      case 1:
+        box1?.change(turnIndex);
+        playOnCubit();
+        break;
+      case 2:
+        box2?.change(turnIndex);
+        playOnCubit();
+        break;
+      case 3:
+        box3?.change(turnIndex);
+        playOnCubit();
+        break;
+      case 4:
+        box4?.change(turnIndex);
+        playOnCubit();
+        break;
+      case 5:
+        box5?.change(turnIndex);
+        playOnCubit();
+        break;
+      case 6:
+        box6?.change(turnIndex);
+        playOnCubit();
+        break;
+      case 7:
+        box7?.change(turnIndex);
+        playOnCubit();
+        break;
+      case 8:
+        box8?.change(turnIndex);
+        playOnCubit();
+        break;
+      default:
+        break;
+    }
   }
 }
